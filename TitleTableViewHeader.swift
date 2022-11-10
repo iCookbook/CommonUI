11d66001eb2ec7d0cda4da5ac6ee72220b85eee1
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-11-10 23:37:04 +0300

    feat: implemented `TitleTableViewHeader`

---
+++ TitleTableViewHeader.swift
@@ -0,0 +1,50 @@
+//
+//  TitleTableViewHeader.swift
+//  CommonUI
+//
+//  Created by Егор Бадмаев on 10.11.2022.
+//
+
+import UIKit
+import Resources
+
+public final class TitleTableViewHeader: UITableViewHeaderFooterView {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = Fonts.title()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - Init
+    
+    public override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Public Properties
+    
+    public func configure(title: String) {
+        titleLabel.text = title
+    }
+    
+    // MARK: - Private Properties
+    
+    private func setupView() {
+        contentView.backgroundColor = Colors.systemBackground
+        contentView.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+}
